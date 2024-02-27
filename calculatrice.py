4779f1f56482ce14d33efb62f7c432a37e02ce4b
patrick tchossiewe <patrickfrank.tchossiewedjengoue.etu@univ-lille.fr>
2024-02-27 12:10:44 +0100

    j'ai rajouté le fichier calculatrice

---
+++ calculatrice.py
@@ -0,0 +1,11 @@
+def sum(number1,number2):
+    return number1 + number2
+
+def main():
+    number1 = int(input("entrez une valeur: "))
+    number2 = int(input("entrez une valeur: "))
+    print(sum(number1,number2))
+
+if __name__ == "__main__":
+    main()
+
